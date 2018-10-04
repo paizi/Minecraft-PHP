f5ae42459244a0a9e0db48b8f84ba1ba0c0b8770
派兹 <admin@blingwang.cn>
2018-10-04 23:22:32 +0800

    Change Bootstrap CDN
    旧cdn已失效

--- view.php
+++ view.php
@@ -77,7 +77,7 @@
 	<meta charset="utf-8">
 	<title>Minecraft Ping PHP Class</title>
 
-   <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<style type="text/css">
 		.jumbotron {
 			margin-top: 30px;

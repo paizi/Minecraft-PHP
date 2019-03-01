fbc5e402afbcb0e80ce5994532f0c24375504edc
派兹 <admin@blingwang.cn>
2019-03-01 21:12:20 +0800

    Format source

--- view_new.php
+++ view_new.php
@@ -1,75 +1,59 @@
 <?php
-	use xPaw\MinecraftPing;
-	use xPaw\MinecraftPingException;
-
-	// 请在此修改服务器信息 ->
-	define( 'MQ_SERVER_ADDR', 'play.craft.moe' );
-	define( 'MQ_SERVER_PORT', 25565 );
-	define( 'MQ_TIMEOUT', 3 );
-	// 请在此修改服务器信息 <-
-
-	// Display everything in browser, because some people can't look in logs for errors
-	Error_Reporting( E_ALL | E_STRICT );
-	Ini_Set( 'display_errors', true );
-
-	require __DIR__ . '/src/MinecraftPing.php';
-	require __DIR__ . '/src/MinecraftPingException.php';
-
-	$Timer = MicroTime( true );
-
-	$Info = false;
-	$Query = null;
-	$count =0;
-	$online =true;
-	try
-	{
-		$Query = new MinecraftPing( MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT );
-		
-		$Info = $Query->Query( );
-		while ( $Info === false )
-		{
-			if( $Query !== null )
-			{
-				$Query->Close( );
-			}
-			$count++;
-			if($count >10)
-			{
-				$online=false;
-				break;
-			}
-			$Query = new MinecraftPing( MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT );
-			$Info = $Query->Query( );
-		}
-		
-		// if( $Info === false )
-		// {
-			// /*
-			 // * If this server is older than 1.7, we can try querying it again using older protocol
-			 // * This function returns data in a different format, you will have to manually map
-			 // * things yourself if you want to match 1.7's output
-			 // *
-			 // * If you know for sure that this server is using an older version,
-			 // * you then can directly call QueryOldPre17 and avoid Query() and then reconnection part
-			 // */
-
-			// $Query->Close( );
-			// $Query->Connect( );
-
-			// $Info = $Query->QueryOldPre17( );
-		// }
-	}
-	catch( MinecraftPingException $e )
-	{
-		$Exception = $e;
-	}
-
-	if( $Query !== null )
-	{
-		$Query->Close( );
-	}
-
-	$Timer = Number_Format( MicroTime( true ) - $Timer, 4, '.', '' );
+usexPawMinecraftPing;
+usexPawMinecraftPingException;
+// 请在此修改服务器信息 ->
+define('MQ_SERVER_ADDR', 'play.craft.moe');
+define('MQ_SERVER_PORT', 25565);
+define('MQ_TIMEOUT', 3);
+// 请在此修改服务器信息 <-
+// Display everything in browser, because some people can't look in logs for errors
+Error_Reporting(E_ALL | E_STRICT);
+Ini_Set('display_errors', true);
+require __DIR__ . '/src/MinecraftPing.php';
+require __DIR__ . '/src/MinecraftPingException.php';
+$Timer = MicroTime(true);
+$Info = false;
+$Query = null;
+$count = 0;
+$online = true;
+try {
+    $Query = new MinecraftPing(MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT);
+    $Info = $Query->Query();
+    while ($Info === false) {
+        if ($Query !== null) {
+            $Query->Close();
+        }
+        $count++;
+        if ($count > 10) {
+            $online = false;
+            break;
+        }
+        $Query = new MinecraftPing(MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT);
+        $Info = $Query->Query();
+    }
+    // if( $Info === false )
+    // {
+    // /*
+    // * If this server is older than 1.7, we can try querying it again using older protocol
+    // * This function returns data in a different format, you will have to manually map
+    // * things yourself if you want to match 1.7's output
+    // *
+    // * If you know for sure that this server is using an older version,
+    // * you then can directly call QueryOldPre17 and avoid Query() and then reconnection part
+    // */
+    // $Query->Close( );
+    // $Query->Connect( );
+    // $Info = $Query->QueryOldPre17( );
+    // }
+    
+}
+catch(MinecraftPingException $e) {
+    $Exception = $e;
+}
+if ($Query !== null) {
+    $Query->Close();
+}
+$Timer = Number_Format(MicroTime(true) - $Timer, 4, '.', '');
 ?>
     <!DOCTYPE html>
     <html>
@@ -92,8 +76,8 @@
         <section class="header">
             <div class="container">
                 <h1 class="header-title">毛线今天炸了吗</h1>
-               	<?php if($online): ?>
-		            	<?php if( $Info !== false ): ?>
+               	<?php if ($online): ?>
+		            	<?php if ($Info !== false): ?>
                 <div class="card">
                     <div class="summary">
                         <div class="icon icon-status-sum up"></div><div style="letter-spacing:-0.5px;"><div class="summary-detail">好耶，没有炸。 </div>
@@ -107,16 +91,20 @@
         <div class="container"><div class="card monitors has-children"><div class="monitors-header"><div class="monitors-header-title">在线人数：<?php echo $Info['players']['online']; ?></div></div>
 				  	 <div class="monitors-header"><div class="monitors-header-title">最大人数：<?php echo $Info['players']['max']; ?></div></div></div></div>
 				            </section>
-				     <?php else: ?>
+				     <?php
+    else: ?>
                   		<div class="card">
                     <div class="summary">
                         <div class="icon icon-status-sum down"></div><div style="letter-spacing:-0.5px;"><div class="summary-detail">坏耶，炸了。 </div>
                     <div class="summary-checktime">响应时间：<?php echo $Timer; ?>s</div>
                 </div>
                 </section>
-			<?php endif; ?>
-		<?php else: ?>
-		<?php endif; ?>
+			<?php
+    endif; ?>
+		<?php
+else: ?>
+		<?php
+endif; ?>
     <section class="footer">
         <div class="container">
             <div class="footer-content">
